3beef222b2c23342428d9733f9f9437e650d22f7
sahu829 <55076453+sahu829@users.noreply.github.com>
2020-11-29 18:17:37 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,13 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  // let paragraph="krishna"
-  const [paragraph, setparagraph] = React.useState("");
-
-  const handleClick = () => {
-    let val =
-      "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
-    setparagraph(val);
-  };
+  const [paragraph, setparagraph] = React.useState(
+    "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+  );
   return (
     <div id="main">
-      <button id="click" onClick={handleClick}>
-        btn
-      </button>
-
-      <p id="para">{paragraph}</p>
+      <button id="click" onClick={() => setparagraph(paragraph)}></button>
+      <paragraph id="para">{paragraph}</paragraph>
     </div>
   );
 }
