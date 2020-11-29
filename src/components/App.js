53a8e6f6e5f6b946109aa187d386899acc24643c
sahu829 <55076453+sahu829@users.noreply.github.com>
2020-11-29 12:05:52 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  // let paragraph="krishna"
+  const [paragraph, setparagraph] = React.useState("");
+
+  const handleClick = () => {
+    let val =
+      "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
+    setparagraph(val);
+  };
   return (
     <div id="main">
-      // Do not alter the main div
+      <p>{paragraph}</p>
+      <button id="click" onClick={handleClick}>
+        btn
+      </button>
     </div>
   );
 }
-
 
 export default App;
