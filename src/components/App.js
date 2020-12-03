65138176232657ac63ef22fd6278275defb21260
sahu829 <55076453+sahu829@users.noreply.github.com>
2020-12-03 20:46:34 +0530

    Change

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,12 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  return <div id="main"></div>;
+  const handleClick = () => {};
+  return (
+    <div id="main">
+      <button id="click" onClick={handleClick}></button>
+    </div>
+  );
 }
 
 export default App;
