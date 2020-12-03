2724b1e743bf6e0f2a5751c6412c7933269ae55e
sahu829 <55076453+sahu829@users.noreply.github.com>
2020-12-03 20:57:05 +0530

    Change

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,18 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  const handleClick = () => {};
+  const [paraGraph, setParaGraph] = React.useState("");
+  const handleClick = () => {
+    setParaGraph(
+      "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+    );
+  };
   return (
     <div id="main">
       <button id="click" onClick={handleClick}>
         btn
       </button>
+      <p id="para">{paraGraph}</p>
     </div>
   );
 }
